152a267f12f23bf5f3bebe9ce1c85b3349e27c4d
jpuf0 <jpuf@jpuf.xyz>
2026-04-26 16:23:15 +0100

    Fix header moving when collapsing sidebar, main page not being full width when sidebar is collapsed

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -14,7 +14,7 @@ const RootLayout = () => (
       <TooltipProvider>
         <AppSidebar variant="inset" />
         <SidebarInset>
-          <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 px-4">
+          <header className="sticky top-0 flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 px-4">
             <SidebarTrigger className="-ml-1" />
           </header>
           <Outlet />
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -21,7 +21,7 @@ function Index() {
   });
 
   return (
-    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+    <div className="container w-full flex flex-col items-center justify-center gap-12 px-4 py-16">
       <div className="flex flex-col items-center gap-2">
         <p className="text-2xl text-white">
           {hello ? hello.data : "Loading query..."}
