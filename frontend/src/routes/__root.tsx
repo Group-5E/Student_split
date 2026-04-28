06b888c9edc69a25f2ca2a200408789463ba1324
jpuf0 <jpuf@jpuf.xyz>
2026-04-28 16:13:57 +0100

    Streamline logic for showing sidebar for logged in users

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,5 @@
-import { useUser } from "@/hooks/useUser";
 import { AppSidebar } from "@/components/app-sidebar";
+import { Show } from "@/components/auth";
 import { ModeToggle } from "@/components/mode-toggle";
 import { ThemeProvider } from "@/components/theme-provider";
 import {
@@ -11,35 +11,31 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
-const RootLayout = () => {
-  const { user } = useUser();
-  return (
-    <>
-      <ThemeProvider defaultTheme="system" storageKey="theme">
-        <SidebarProvider>
-          <TooltipProvider>
-            {!user ?
-              null :
-              <div>
-                <AppSidebar variant="inset" />
+const RootLayout = () => (
+  <>
+    <ThemeProvider defaultTheme="system" storageKey="theme">
+      <SidebarProvider>
+        <TooltipProvider>
+          <Show when="signed-in">
+            <AppSidebar variant="inset" />
+          </Show>
+          <SidebarInset>
+            <header className="sticky top-0 flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 px-4">
+              <Show when="signed-in">
                 <SidebarTrigger className="-ml-1" />
-              </ div>
-            }
-            <SidebarInset>
-              <header className="sticky top-0 flex h-12 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 px-4">
-                <div className="ml-auto">
-                  <ModeToggle />
-                </div>
-              </header>
-              <Outlet />
-            </SidebarInset>
-          </TooltipProvider>
-        </SidebarProvider>
-      </ThemeProvider>
-      <TanStackRouterDevtools position={"bottom-right"} />
-    </>
-  );
-};
+              </Show>
+              <div className="ml-auto">
+                <ModeToggle />
+              </div>
+            </header>
+            <Outlet />
+          </SidebarInset>
+        </TooltipProvider>
+      </SidebarProvider>
+    </ThemeProvider>
+    <TanStackRouterDevtools position={"bottom-right"} />
+  </>
+);
 
 const NotFound = () => (
   <div className="flex flex-col justify-center items-center h-full">
